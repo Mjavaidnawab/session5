81dbe0e2fbe63fd4645b20425be1aa3d7fdf8cac
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-08-28 18:34:30 +0400

    Create counter context and counter reducer

---
+++ src/CounterReducer.js
@@ -0,0 +1,12 @@
+import counterContext from "./CounterContext"
+
+const counterReducer = (state, action) =>{
+    switch (action) {
+        case 'INCREMENT':
+            return state + 1
+ 
+
+    }
+
+}
+export default counterReducer;
